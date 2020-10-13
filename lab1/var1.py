c4ffeac43e13cca2968d02344b57c16ebe877bb1
seniorkot <seniorkot24@gmail.com>
2020-10-13 06:14:17 +0300

    Color the area of interest border with RGB

--- lab1/var1.py
+++ lab1/var1.py
@@ -5,6 +5,7 @@
 
 import cv2
 import numpy as np
+import time
 
 
 def gstreamer_pipeline(capture_width=1280,
@@ -12,7 +13,7 @@ def gstreamer_pipeline(capture_width=1280,
                        display_width=1280,
                        display_height=720,
                        framerate=30,
-                       flip_method=0,):
+                       flip_method=0):
     """ Returns a correct GStreamer pipeline for NVIDIA CSI camera """
     return (
         "nvarguscamerasrc ! "
@@ -34,13 +35,30 @@ def gstreamer_pipeline(capture_width=1280,
     )
 
 
-def show_camera(h_sensitivity: int = 20,
+def show_frame(frame, start_time,
+               upper_left: tuple,
+               bottom_right: tuple,
+               color: tuple = (0, 255, 255)) -> None:
+    # Draw green rectangle (square)
+    cv2.rectangle(frame, upper_left, bottom_right, color,
+                  thickness=2)
+    # Print processing time
+    cv2.putText(frame, "Time: {:.4f}sec"
+                .format(time.time() - start_time),
+                org=(10, 50), color=(255, 0, 0), thickness=2,
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1)
+    # Show the frame
+    cv2.imshow('frame', frame)
+
+
+def show_camera(fps: int = 30,  # framerate
+                h_sensitivity: int = 20,
                 s_lower: int = 70,
                 s_higher: int = 255,
                 v_lower: int = 50,
                 v_higher: int = 255):
-    # print(gstreamer_pipeline(flip_method=4))
-    # cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=4),
+    # print(gstreamer_pipeline(flip_method=4, framerate=fps))
+    # cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=4, framerate=fps),
     #                        cv2.CAP_GSTREAMER)
     cap = cv2.VideoCapture(0)
     if cap.isOpened():
@@ -50,6 +68,12 @@ def show_camera(h_sensitivity: int = 20,
         # Check fullscreen window property to determine if it's still available
         while cv2.getWindowProperty("MCV Lab_1 Var_1", 0) >= 0:
             ret, frame = cap.read()
+
+            # Check return value (in case no frames available)
+            if not ret:
+                break
+
+            start_time = time.time()
 
             # Step 1 - Get coords of the area of interest
             height, width, _ = frame.shape
@@ -66,48 +90,47 @@ def show_camera(h_sensitivity: int = 20,
             lower_red = np.array([0, s_lower, v_lower])
             upper_red = np.array([h_sensitivity, s_higher, v_higher])
             mask1 = cv2.inRange(hsv, lower_red, upper_red)
-
             # Range for upper red
             lower_red = np.array([180 - h_sensitivity, s_lower, v_lower])
             upper_red = np.array([180, s_higher, v_higher])
             mask_red = mask1 + cv2.inRange(hsv, lower_red, upper_red)
+
             # Crop mask with the area of interest
             mask_red = mask_red[upper_left[1]:bottom_right[1] + 1,
                                 upper_left[0]:bottom_right[0] + 1]
-            if 0 not in mask_red:
-                pass
-                # TODO: do an action
 
             # Create mask for green color
             lower_green = np.array([60 - h_sensitivity, s_lower, v_lower])
             upper_green = np.array([60 + h_sensitivity, s_higher, v_higher])
             mask_green = cv2.inRange(hsv, lower_green, upper_green)
+
             # Crop mask with the area of interest
             mask_green = mask_green[upper_left[1]:bottom_right[1] + 1,
                                     upper_left[0]:bottom_right[0] + 1]
-            if 0 not in mask_green:
-                pass
-                # TODO: do an action
 
             # Create mask for blue color
             lower_blue = np.array([120 - h_sensitivity, s_lower, v_lower])
             upper_blue = np.array([120 + h_sensitivity, s_higher, v_higher])
             mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+
             # Crop mask with the area of interest
             mask_blue = mask_blue[upper_left[1]:bottom_right[1] + 1,
                                   upper_left[0]:bottom_right[0] + 1]
-            if 0 not in mask_blue:
-                pass
-                # TODO: do an action
 
-            # Draw green rectangle (square) and show the frame
-            cv2.rectangle(frame, upper_left, bottom_right, (0, 255, 0),
-                          thickness=1)
-            cv2.imshow('frame', frame)
-
-            # Stop the program on the ESC key
+            # Stop the program on the ESC key or check masks
             if cv2.waitKey(1) & 0xFF == 27:
                 break
+            elif 0 not in mask_red:
+                show_frame(frame, start_time, upper_left, bottom_right,
+                           color=(0, 0, 255))
+            elif 0 not in mask_green:
+                show_frame(frame, start_time, upper_left, bottom_right,
+                           color=(0, 255, 0))
+            elif 0 not in mask_blue:
+                show_frame(frame, start_time, upper_left, bottom_right,
+                           color=(255, 0, 0))
+            else:
+                show_frame(frame, start_time, upper_left, bottom_right)
 
         cap.release()
         cv2.destroyAllWindows()
