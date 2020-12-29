845f13ec04130a5f1a53bb3369c658337c386185
seniorkot <seniorkot24@gmail.com>
2020-12-29 11:19:27 +0700

    Finish image classifier

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -2,35 +2,75 @@ import sys
 import getopt
 import time
 import torch
-from PIL import Image
+import csv
+from pathlib import Path
+from PIL import Image, ImageDraw, ImageFont
+from torch.autograd import Variable
+from torchvision import transforms
 from torchvision.models.alexnet import alexnet
 from torch2trt import torch2trt
 from torch2trt import TRTModule
+
+
+# Create dictionary with classes
+with open('./config/classes.csv', 'r') as fd:
+    dc = csv.DictReader(fd)
+    classes = {}
+    for line in dc:
+        classes[int(line['class_id'])] = line['class_name']
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+transform = transforms.Compose([transforms.Resize(256),
+                                transforms.CenterCrop(224),
+                                transforms.ToTensor()])
+
+Path("./output").mkdir(exist_ok=True)
 
 
 def process_images(images: list,
                    trt: bool):
     if trt:
         timest = time.time()
-        x1 = torch.ones((1, 3, 224, 224)).cuda()
-        model = alexnet(pretrained=True).eval().cuda()
-        model_trt = torch2trt(model, [x1])
-        torch.save(model_trt.state_dict(), 'alexnet_trt.pth')
+        # x1 = torch.ones((1, 3, 224, 224)).cuda()
+        # model = alexnet(pretrained=True).eval().cuda()
+        # model_trt = torch2trt(model, [x1])
+        # torch.save(model_trt.state_dict(), 'alexnet_trt.pth')
         # exit()
-        # model = TRTModule()
-        # model.load_state_dict(torch.load('alexnet_trt.pth'))
-        print("load time {}".format(time.time() - timest))
+        model = TRTModule()
+        model.load_state_dict(torch.load('alexnet_trt.pth'))
+        print("Model load time {}".format(time.time() - timest))
     else:
         timest = time.time()
         model = alexnet(pretrained=True).eval().cuda()
-        print("load time {}".format(time.time() - timest))
+        print("Model load time {}".format(time.time() - timest))
+
+    for image in images:
+        index = classify_image(image, model)
+        output_text = str(index) + ': ' + classes[index]
+        edit = ImageDraw.Draw(image)
+        edit.rectangle((0, image.height - 20, image.width, image.height),
+                       fill=(255, 255, 255))
+        edit.text((50, image.height-15), output_text, (0, 0, 0),
+                  font=ImageFont.load_default())
+        image.save('./output/' + image.filename.split('/')[-1])
+
+
+def classify_image(image: Image,
+                   model) -> int:
+    image_tensor = transform(image).float()
+    image_tensor = image_tensor.unsqueeze_(0)
+    input = Variable(image_tensor).to(device)
+
+    # Predict image class
+    timest = time.time()
+    output = model(input)
+    print("Image processing time {}".format(time.time() - timest))
+    return output.data.cpu().numpy().argmax()
 
 
 def print_usage():
-    """
-    Prints script usage.
-    :return: None
-    """
     print("Usage: python lab3.py [--trt] <image> [image...]")
 
 
@@ -48,11 +88,6 @@ def main(argv: list,
         print_usage()
         sys.exit(1)
 
-    # Exit with error if no images provided
-    if len(argv) == 0:
-        print_usage()
-        sys.exit(1)
-
     # Open images
     images = []
     for img_path in argv:
@@ -61,6 +96,11 @@ def main(argv: list,
             images.append(image)
         except FileNotFoundError:
             print(img_path + " not found")
+
+    # Exit with error if there is no images
+    if len(images) == 0:
+        print_usage()
+        sys.exit(1)
 
     process_images(images, trt)
 
