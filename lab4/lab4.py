8807d96e4907bb50c27b19acf9ca877cb6d8a5c6
seniorkot <seniorkot24@gmail.com>
2021-01-12 12:44:40 +0700

    Add KNN classifier

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -10,7 +10,9 @@ import time
 
 import cv2
 import pickle
-import face_recognition
+import math
+import face_recognition as fcr
+from sklearn import neighbors
 import numpy as np
 
 
@@ -41,6 +43,57 @@ def gstreamer_pipeline(capture_width=1280,
     )
 
 
+def show_frame(knn_clf: neighbors.KNeighborsClassifier,
+               video=None):
+    if video is not None:
+        print('Not none')
+        cap = cv2.VideoCapture(video)
+    else:
+        # print(gstreamer_pipeline(flip_method=4))
+        # cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=4),
+        #                        cv2.CAP_GSTREAMER)
+        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+    if cap.isOpened():
+        # Create a window
+        _ = cv2.namedWindow("MCV Lab 4", cv2.WINDOW_AUTOSIZE)
+
+        # Check fullscreen window property to determine if it's still available
+        while cv2.getWindowProperty("MCV Lab 4", 0) >= 0:
+            ret, frame = cap.read()
+
+            # Check return value (in case no frames available)
+            if not ret:
+                break
+
+            # Resize to increase face detection computations
+            imgS = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+            face_locations = fcr.face_locations(imgS)
+            if len(face_locations) > 0:
+                predictions = predict(knn_clf, imgS, face_locations)
+
+                # # for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+                # for faceLoc in face_locations:
+                #     y1, x2, y2, x1 = faceLoc
+                #     y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                #     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                #     # cv2.rectangle(frame, (x1, y2 - 35), (x2, y2), (0, 255, 0),
+                #     #               cv2.FILLED)
+
+            # Stop the program on the ESC key or check masks
+            if cv2.waitKey(1) & 0xFF == 27:
+                break
+            else:
+                cv2.imshow('frame', frame)
+
+        cap.release()
+        cv2.destroyAllWindows()
+    else:
+        print("Unable to open camera")
+
+
 def get_encodings(data_path: str) -> dict:
     encoding_path = os.path.join(data_path, 'encodings.data')
     encodings = {}
@@ -49,14 +102,73 @@ def get_encodings(data_path: str) -> dict:
         with open(encoding_path, 'rb') as f:
             encodings = pickle.load(f)
     else:
+        timest = time.time()
         for img in os.listdir(data_path):
             image = cv2.imread(f'{data_path}/{img}', )
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            encode = face_recognition.face_encodings(image)[0]
-            encodings[os.path.splitext(img)[0]] = encode
+            locations = fcr.face_locations(image)
+            if len(locations) != 1:
+                print("Image {} not suitable for training: {}"
+                      .format(img, "Didn't find a face" if len(locations) < 1
+                              else "Found more than one face"))
+            else:
+                encode = fcr.face_encodings(image)[0]
+                encodings[os.path.splitext(img)[0]] = encode
+        print(f'Completed encoding of {len(encodings)} faces in '
+              f'{time.time() - timest} sec')
         with open(encoding_path, 'wb') as f:
             pickle.dump(encodings, f)
     return encodings
+
+
+def train(data_path: str,
+          model_path=None,
+          n_neighbors=None) -> neighbors.KNeighborsClassifier:
+    if model_path is not None and os.path.isfile(model_path):
+        with open(model_path, 'rb') as f:
+            knn_clf = pickle.load(f)
+    else:
+        encodings = get_encodings(data_path)
+        X = list(encodings.values())
+        y = list(encodings.keys())
+
+        if n_neighbors is None:
+            n_neighbors = int(round(math.sqrt(len(X))))
+            print("Chose n_neighbors automatically:", n_neighbors)
+
+        timest = time.time()
+        # Create and train the KNN classifier
+        knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors,
+                                                 algorithm='ball_tree',
+                                                 weights='distance')
+        knn_clf.fit(X, y)
+        print(f'Completed classifier training in {time.time() - timest} sec')
+
+        # Save the trained KNN classifier
+        if model_path is not None:
+            with open(model_path, 'wb') as f:
+                pickle.dump(knn_clf, f)
+
+    return knn_clf
+
+
+def predict(knn_clf: neighbors.KNeighborsClassifier,
+            img,
+            face_locations,
+            distance_threshold=0.6):
+    # Find encodings for faces in the test iamge
+    face_encodings = fcr.face_encodings(img, face_locations)
+
+    # Use the KNN model to find the best matches for the test face
+    closest_distances = knn_clf.kneighbors(face_encodings, n_neighbors=1)
+    are_matches = [closest_distances[0][i][0] <= distance_threshold
+                   for i in range(len(face_locations))]
+
+    # Predict classes and remove classifications that aren't
+    # within the threshold
+    return [(pred, loc) if rec else ("unknown", loc)
+            for pred, loc, rec in zip(knn_clf.predict(face_encodings),
+                                      face_locations, are_matches)]
 
 
 def print_usage(exit_code: int):
@@ -89,12 +201,13 @@ def main(argv: list,
         else:
             print_usage(1)
 
-    # Encode faces
-    timest = time.time()
-    encodings = get_encodings(data_path)
-    print(f'Completed encoding of {len(encodings)} faces in '
-          f'{time.time() - timest} sec')
-    # TODO: Show camera or video, locate faces, etc.
+    # Train classifier
+    knn_clf = train(data_path)
+    if knn_clf is None:
+        print('Classifier is not trained')
+        sys.exit(3)
+
+    show_frame(knn_clf)
 
 
 if __name__ == "__main__":
